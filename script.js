31b15240357316592a48480129f959af0d50d6ec
渡海 <erlanggamuhammad94@gmail.com>
2023-03-05 11:14:35 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// const url = 'https://api.adviceslip.com/advice';
+let id = document.getElementById('label');
+let advice = document.getElementById('advice');
+
+async function getRand(url) {
+    try {
+        let rawData = await fetch(url);
+        let textData = await rawData.text();
+        let arrData = await JSON.parse(textData);
+        id.innerHTML = "ADVICE #" + arrData.slip.id;
+        advice.innerHTML = arrData.slip.advice;
+        return console.log(arrData.slip);
+    } catch (error) {
+        return console.log(error);
+    }
+}
